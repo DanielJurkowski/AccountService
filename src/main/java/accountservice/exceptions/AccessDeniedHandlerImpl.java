b04a9f619ca5ca8458e123313d473fbbb52aabed
Daniel <danieljurkowski.6@gmail.com>
2022-10-29 23:20:39 +0200

    constructor

--- src/main/java/accountservice/exceptions/AccessDeniedHandlerImpl.java
+++ src/main/java/accountservice/exceptions/AccessDeniedHandlerImpl.java
@@ -4,29 +4,25 @@ import accountservice.auditor.AuditorService;
 import accountservice.security.Event;
 import accountservice.security.SecurityEvent;
 import accountservice.user.CurrentUser;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@AllArgsConstructor
 public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
     private final AuditorService auditorService;
     private final CurrentUser currentUser;
 
-    public AccessDeniedHandlerImpl(AuditorService auditorService, CurrentUser currentUser) {
-        this.auditorService = auditorService;
-        this.currentUser = currentUser;
-    }
-
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         auditorService.saveSecurityEvent(SecurityEvent
                 .builder()
                 .action(Event.ACCESS_DENIED)
